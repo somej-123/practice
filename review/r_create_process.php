017c296b55020c30d79ae10fff5d5b9f2a3a72d6
unknown <somej@naver.com>
2020-11-15 02:37:43 +0900

    테스트 20/11/15

--- review/r_create_process.php
+++ review/r_create_process.php
@@ -1,9 +1,4 @@
 <?php
-    if(isset($_POST['title']) == '.'){
-    $_POST['title'] = str_replace('.',$_POST['title']);
     file_put_contents('reviewData/'.$_POST['title'],$_POST['content']);
-    }else{
-    file_put_contents('reviewData/'.$_POST['title'],$_POST['content']);
-    }
     header("Location:review.php?id=".$_POST['title']);
 ?>
